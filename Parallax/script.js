9a1b1555323d23bfa24f5fe2ac417bfdffd01c05
Nabil Loussaifi <82761487+nabilloussaifi@users.noreply.github.com>
2021-09-15 10:52:27 +0200

    Add files via upload

---
+++ Parallax/script.js
@@ -0,0 +1,11 @@
+document.addEventListener("mousemove", parallax);
+function parallax(e){
+  document.querySelectorAll(".image").forEach(function(move){
+
+    let moving_value = move.getAttribute("data-value");
+    let x = (e.clientX * moving_value) / 250;
+    let y = (e.clientY * moving_value) / 250;
+
+    move.style.transform = "translateX(" + x + "px) translateY(" + y + "px)";
+  });
+}
